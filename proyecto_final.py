723cbd3e4767480c0d08b5d208d14a4a808804e0
Ramila <rami061201@gmail.com>
2024-12-01 01:38:07 -0600

    add some changes

--- proyecto_final.py
+++ proyecto_final.py
@@ -11,5 +11,6 @@ def health():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
+
 
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from proyecto_final import app  # Asegúrate de que 'proyecto_final' sea tu aplicación Flask
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
+
 
